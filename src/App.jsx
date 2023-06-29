a7724f617808a8fbf66e32639280069c661cdb98
Salvo Falcone <salvofalcone57@gmail.com>
2023-06-29 10:33:16 -0300

    feat: created navbar and contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,53 @@
 // import cocktailLogo from "./assets/cocktail.svg";
 import { useState } from "react";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import ProductSection from "./components/productSection";
 import ProductList from "./components/productList";
+import Contacts from "./components/contacts";
 
 import "./App.css";
-import "swiper/css";
-import "swiper/css/pagination";
 
 function App() {
   const [productSection, setProductSection] = useState("");
 
+  const onRender = () => {
+    switch (productSection) {
+      case "contacts":
+        return <Contacts />;
+      case "":
+        return (
+          <>
+            <ProductList name="rum" setProductSection={setProductSection} />
+            <ProductList name="vodka" setProductSection={setProductSection} />
+            <ProductList name="gin" setProductSection={setProductSection} />
+          </>
+        );
+      default:
+        return (
+          <ProductSection
+            productSection={productSection}
+            setProductSection={setProductSection}
+          />
+        );
+    }
+  };
+
   return (
     <div className="App">
-      {productSection ? (
-        <ProductSection
-          productSection={productSection}
-          setProductSection={setProductSection}
-        />
-      ) : (
-        <>
-          <ProductList name="rum" setProductSection={setProductSection} />
-          <ProductList name="vodka" setProductSection={setProductSection} />
-          <ProductList name="gin" setProductSection={setProductSection} />
-        </>
-      )}
+      <nav>
+        <ul className="Nav__List">
+          <li onClick={() => setProductSection("")} className="Nav__List--Item">
+            Home
+          </li>
+          <li
+            onClick={() => setProductSection("contacts")}
+            className="Nav__List--Item">
+            Contact Us
+          </li>
+        </ul>
+      </nav>
+
+      {onRender()}
     </div>
   );
 }
